72be0597dd03261a8d243d0a514858c38da0e678
Joshua Cruey <Joshua@crueyphotography.com>
2016-06-19 20:38:55 -0400

    working on inquirer prompts

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,6 +1,7 @@
+// Node npm var declarations
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-
+//creates a connection to MySQL database
 var connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
@@ -8,63 +9,70 @@ var connection = mysql.createConnection({
 	password: 'Fl0r1d@!',
 	database: 'Bamazon'
 })
-
+//Provides status of SQL connection
 connection.connect(function(err) {
 	if (err) throw err;
 	console.log('connected as id' + connection.threadId);
 	start();
 })
-
+// performs inital query of Products table from database
 var start = function() {
     connection.query('SELECT * FROM Products', function(err, res) {
         console.log('---------------------------------');
         console.log('Available Bamazon Products');
-        console.log('---------------------------------');
+        console.log('---------------------------------\n');
         for (var i=0; i < res.length; i++) {
-		console.log(res[i].ItemID + " || " + res[i].ProductName + " || " + res[i].Price + " || " + res[i].stockQuantity);	
+		console.log(res[i].ItemID + " || " + res[i].ProductName + " || Price: " + res[i].Price + " || Quantity: " + res[i].stockQuantity + '\n');	
 		}
         buyItem();
         })
     }
-
+//Prompts the customer on which item to buy
 var buyItem = function() {
-	connection.query('SELECT * FROM Products', function(err, res) {
 	inquirer.prompt({
-		name: "choice",
-		type: "rawlist",
-		choices: function(value) {
-			var choiceArray = [];
-			for (var i = 0; i < res.length; i++) {
-                    choiceArray.push(res[i].ProductName +': '+ res[i].Price);
-                }
-                return choiceArray;
-            },
-        	message: "Choose the ID of the Item you would like to buy"
-		}).then(function(answer) {
-			for (var i = 0; i < res.length; i++) {
-	                if (res[i].ItemID == answer.choice) {
-	                    var chosenItem = res[i];
-                        inquirer.prompt({
-                        	name: "buy",
-                        	type: "input",
-                        	message: "How many would you like to buy?"
-                    }).then(function(answer) {
-                        if (chosenItem.stockQuantity < parseInt(answer.buy)) {
-                            connection.query("UPDATE Products SET ? WHERE ?", [{
-                                stockQuantity: answer.buy
-                            }, {
-                                id: chosenItem.id
-                            }], function(err, res) {
-                                console.log("Purchase successfull!");
-                                start();
-                            });
-                        } else {
-                            console.log("We're sorry, that Item is currently out of stock");
-                            start();
-                        }
-                    })
-                }
+		name: "Item",
+		type: "input",
+        message: "Choose the ID of the Item you would like to buy",
+		validate: function(value) {
+			
+            if (isNaN(value) == false) {
+                return true;
+            } else {
+                console.log("\nPlease enter only the Item number of the item you'd like to buy\n");
+                return false;
             }
-        })
-	})
-}
+        }
+    }, {
+        name: "Qty",
+        type: "input",
+        message: "How many would you like to buy?",
+        validate: function(value) {
+            
+            if (isNaN(value) == false) {
+                return true;
+            } else {
+                console.log("\nPlease enter a valid Quantity\n");
+                return false;
+            }
+        }
+        }).then(function(answer) {
+			console.log(answer);
+            var ItemInt = parseInt(answer.Qty);
+            // console.log(ItemInt);
+            //     connection.query("SELECT * FROM Products WHERE ?", [{ItemID: answer}], function(err, data) { 
+            //         if (err) throw err;
+            //         if (data[0].stockQuantity < ItemInt) {
+            //            console.log("We're sorry, that Item is currently out of stock");
+            //            start(); 
+            //         } else {
+            //             var updateQty = data[0].stockQuantity - ItemInt
+            //         console.log("Purchase successfull!");
+            //         start();
+                // }               
+            });
+        // });
+    };
+
+
+
+
