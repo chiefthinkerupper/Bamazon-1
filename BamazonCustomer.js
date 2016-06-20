42a027d41b5499fb683723b9e9eb578f488baff7
Joshua Cruey <Joshua@crueyphotography.com>
2016-06-20 00:02:41 -0400

    working customer app

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -22,14 +22,15 @@ var start = function() {
         console.log('Available Bamazon Products');
         console.log('---------------------------------\n');
         for (var i=0; i < res.length; i++) {
-		console.log(res[i].ItemID + " || " + res[i].ProductName + " || Price: " + res[i].Price + " || Quantity: " + res[i].stockQuantity + '\n');	
+		console.log(res[i].ItemID + " || " + res[i].ProductName + " || Price: " + res[i].Price + " || Quantity: " + res[i].StockQuantity + '\n');	
 		}
         buyItem();
         })
     }
+
 //Prompts the customer on which item to buy
 var buyItem = function() {
-	inquirer.prompt({
+	inquirer.prompt([{
 		name: "Item",
 		type: "input",
         message: "Choose the ID of the Item you would like to buy",
@@ -55,22 +56,45 @@ var buyItem = function() {
                 return false;
             }
         }
-        }).then(function(answer) {
+        }]).then(function(answer) {
 			console.log(answer);
             var ItemInt = parseInt(answer.Qty);
-            // console.log(ItemInt);
-            //     connection.query("SELECT * FROM Products WHERE ?", [{ItemID: answer}], function(err, data) { 
-            //         if (err) throw err;
-            //         if (data[0].stockQuantity < ItemInt) {
-            //            console.log("We're sorry, that Item is currently out of stock");
-            //            start(); 
-            //         } else {
-            //             var updateQty = data[0].stockQuantity - ItemInt
-            //         console.log("Purchase successfull!");
-            //         start();
-                // }               
+            console.log(ItemInt);
+                connection.query("SELECT * FROM Products WHERE ?", [{ItemID: answer.Item}], function(err, data) { 
+                    if (err) throw err;
+                    if (data[0].StockQuantity < ItemInt) {
+                       console.log("We're sorry, that Item is currently out of stock\n");
+                       console.log("Please choose another Product\n");
+                       start(); 
+                    } else {
+                        var updateQty = data[0].StockQuantity - ItemInt;
+                        var totalPrice = data[0].Price * ItemInt;
+                        connection.query('UPDATE products SET StockQuantity = ? WHERE ItemID = ?', [updateQty, answer.Item], function(err, results) {
+                        if(err) {
+                            throw err;
+                        } else {
+                        console.log("Purchase successfull!\n");
+                        console.log("Your total cost is: $ " + totalPrice);
+                        inquirer.prompt({
+                            name: "buyMore",
+                            type: "confirm",
+                            message: "Would you like to buy another Product? [Y/N]",
+                            validate: function(value) {
+                                if (value == Y) {
+                                    return true;
+                                    start();
+                                } else {
+                                    return false;
+                                    connection.end();
+                                    console.log("Thank you for shopping with Bamazon!")
+                                }
+                            }
+                        })
+                        }
+                    });
+                }               
             });
-        // });
+        });
     };
 
 
